74fafcd88df89224a7ac3150e924e6ed5e2499a1
Polina Tyureva <polina.tyureva@devexpress.com>
2023-11-22 20:57:38 +0400

    update example

---
+++ CS/Global.asax.cs
@@ -0,0 +1,10 @@
+﻿using DashboardGridHyperlink.Models;
+using System;
+
+namespace DashboardGridHyperlink {
+    public class Global : System.Web.HttpApplication {
+        protected void Application_Start(object sender, EventArgs e) {
+            DevExpress.Utils.DeserializationSettings.RegisterTrustedClass(typeof(SalesPersonData));
+        }
+    }
+}
